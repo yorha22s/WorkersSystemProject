7044d342daf9fdabef921283bf2d5f33d5d315ef
yorha22b <jkantorowicz05@gmail.com>
2023-05-29 20:14:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+    }
+}
