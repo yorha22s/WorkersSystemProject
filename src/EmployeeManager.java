e6069939735a12879612b8d55dcbfd97176d95b5
yorha22b <jkantorowicz05@gmail.com>
2023-05-29 20:19:48 +0200

    Created class EmployeeManager

---
+++ src/EmployeeManager.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class EmployeeManager {
+
+    private final HashMap<Integer,Employee> employees;
+
+    public EmployeeManager(HashMap<Integer, Employee> employees) {
+        this.employees = employees;
+    }
+
+}
