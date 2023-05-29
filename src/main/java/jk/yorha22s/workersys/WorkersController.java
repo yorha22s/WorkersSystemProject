2d399a916b5218fc2d29f983506a5088e49cd68a
yorha22b <jkantorowicz05@gmail.com>
2023-05-29 21:53:42 +0200

    Empty spring boot app

---
+++ src/main/java/jk/yorha22s/workersys/WorkersController.java
@@ -0,0 +1,9 @@
+package jk.yorha22s.workersys;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkersController {
+    //https://www.baeldung.com/thymeleaf-variables
+}
---
+++ src/test/java/jk/yorha22s/workersys/WorkersysApplicationTests.java
@@ -0,0 +1,13 @@
+package jk.yorha22s.workersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
