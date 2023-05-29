53be37a6ef7090e013c38520816d7a0baa2e2496
yorha22b <jkantorowicz05@gmail.com>
2023-05-29 20:16:15 +0200

    Created class Employee

---
+++ src/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int employeeID;
+    private int salary;
+
+    public Employee(String firstName, String lastName, String position, int employeeID, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.employeeID = employeeID;
+        this.salary = salary;
+    }
+}
