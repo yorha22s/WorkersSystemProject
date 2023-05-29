f43782be3226327e3f16a173e2dec879272011ef
yorha22b <jkantorowicz05@gmail.com>
2023-05-29 20:20:30 +0200

    Implemented method removeEmployee

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -8,4 +8,15 @@ public class EmployeeManager {
         this.employees = employees;
     }
 
+    public  void addEmployee(Employee employee){
+        Integer keyEmployee = employee.getEmployeeID();
+        employees.put(keyEmployee,employee);
+    }
+
+    public void removeEmployee(Employee employee){
+        if(employees.containsKey(employee.getEmployeeID()))
+            employees.remove(employee.getEmployeeID());
+        else
+            System.out.println("Pracownik o takim ID nie istnieje");
+    }
 }
